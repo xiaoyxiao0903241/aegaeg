de172f0e2c8addb57eccec01282a21290b4310c4
iZzu3cfkmecu77Z\Administrator <upmanu04@gmail.com>
2026-07-03 01:27:00 +0800

    rpc

--- src/web3/bsc-read-client.ts
+++ src/web3/bsc-read-client.ts
@@ -4,5 +4,5 @@ import { bsc } from 'viem/chains'
 /** Read-only BSC client for QuoterV2 simulate calls (thirdweb readContract cannot quote V3). */
 export const bscReadClient = createPublicClient({
   chain: bsc,
-  transport: http('https://bsc-dataseed1.binance.org'),
+  transport: http('https://bnb-mainnet.g.alchemy.com/v2/yU_7sTSg_x8TMWQI36unf'),
 })
